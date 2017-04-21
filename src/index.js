8617d828be65d7dfd4f67976d878b59c2c985577
Offirmo <offirmo.net@gmail.com>
2017-04-21 17:35:40 +1000

    TLTWGCMOMPP :scream_cat:

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ console.log('Hello world from the JS app!')
 const PORT = process.env.PORT || 5000
 const FOO = process.env.FOO || 33
 
-
-function requestHandler(request, response) {
-	response.end('Hello from node.js Server!')
-}
-
-const server = http.createServer(requestHandler)
+// https://blog.risingstack.com/your-first-node-js-http-server/
+const server = http.createServer(function requestHandler(request, response) {
+	response.end(`Hello from node.js!
+FOO = ${FOO}
+dice = ${_.random(1, 6)}
+`
+	)
+})
 
 server.listen(PORT, (err) => {
 	if (err) {
